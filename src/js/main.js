2fe24f63dd35f8baf544261222d173c6acf83047
lfernanda <lfernanda1192@gmail.com>
2024-05-26 15:52:34 -0500

    Agregando imagenes de turismo, pagina de turismo, pasarela ( con sus respectivos html,js y estilos)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,30 @@
-// Import our custom CSS
+//Maneja todos los archivos js usados en el proyecto
 import '../scss/styles.scss'
-
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import {routes} from "./routes.js"
+import { infoCommunity } from './planCommunity.js'
+import {stripeCheckout} from './stripe-checkout.js'
+
+routes();
+
+setTimeout(() => {
+    stripeCheckout(".container-cards-plan");
+    }, 100);
+
+
+if (window.location.pathname === '/plans') {
+    setTimeout(() => {
+    infoCommunity(".plan-community")
+    }, 100);
+}
+ 
+
+
+
+
+
+
+
+
+
+
---
+++ src/js/stripe-keys.js
@@ -0,0 +1,5 @@
+//Llaver necesarias para acceder a stripe
+export default {
+    public: 'pk_test_51Mg5xgBrpCweGOwO5WyzFtab5GW10YJEKpbW9beEpXma9INwmEBTeEz3Nk8k4WbUV1J6zmyyiuUpje5UMi7MV2KH00fWNKsKM9',
+    secret: 'sk_test_51Mg5xgBrpCweGOwOoZqZfoS9rJSGAK2g6peB2oGlWfd4AdQ88PyZHoQ8RUjxfGbUeD234Zy5HPlP38kvwebmWcC700KvcZw834'
+  };
