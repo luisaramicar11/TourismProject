7ef729e860816360a9ea41264eb4a73f24ecb712
lfernanda <lfernanda1192@gmail.com>
2024-05-26 23:28:39 -0500

    agregando diccionario

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import { infoCommunity } from './planCommunity.js'
 import {stripeCheckout} from './stripe-checkout.js'
 import { putComments } from './home.js'
 import Swal from 'sweetalert2'
+//import {translate} from "./translate.js"
 
 // Selecting input fields from the form
 
@@ -16,6 +17,38 @@ setTimeout(() => {
     stripeCheckout(".container-cards-plan");
     putComments("#slice-1", "#slice-2");
     }, 1000);
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const languageSelector = document.getElementById('language-selector');
+        languageSelector.addEventListener('change', (event) => {
+          const selectedLanguage = event.target.value;
+          loadLanguage(selectedLanguage);
+        });
+      
+        // Load default language (Spanish)
+        loadLanguage('es');
+      });
+      
+      function loadLanguage(language) {
+        fetch(`public//locales/${language}/translation.json`)
+          .then(response => response.json())
+          .then(translations => {
+            updateTextContent(translations);
+          })
+          .catch(error => console.error('Error loading language:', error));
+      }
+      
+      function updateTextContent(translations) {
+        document.querySelectorAll('[data-i18n]').forEach(element => {
+          const key = element.getAttribute('data-i18n');
+          if (translations[key]) {
+            element.textContent = translations[key];
+          }
+        });
+      }
+      
+
+//translate('[data-i18n]')    
 
 if (window.location.pathname === '/plans') {
     setTimeout(() => {
