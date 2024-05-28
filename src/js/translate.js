10fc621965c2d8551bd5b97c57d96be6d2b293b5
arlexz96 <arlex.z96@gmail.com>
2024-05-28 14:17:50 -0500

    "Feat: Se agregan traducciones de las vistas Nosotros, Comunidades y plansTourism"

--- src/js/translate.js
+++ src/js/translate.js
@@ -2,39 +2,39 @@
 import i18next from 'i18next';
 import Backend from 'i18next-http-backend'
 
-export function translate(datas){
+export function translate(datas) {
     let language;
     let allDatas = document.querySelectorAll(datas)
 
-if(localStorage.getItem('language')){
-    language = localStorage.getItem('language')
-}else{
-    language = 'en'
-}
+    if (localStorage.getItem('language')) {
+        language = localStorage.getItem('language')
+    } else {
+        language = 'en'
+    }
 
-i18next.use(Backend).init({
-  lng: language, // if you're using a language detector, do not define the lng option
-  debug: true,
-  backend: {
-    loadPath: '/locales/{{lng}}/{{ns}}.json'
-  },
-  ns:['translation'],
-  defaultNS: 'translation'
-}).then(() => updateContent())
+    i18next.use(Backend).init({
+        lng: language, // if you're using a language detector, do not define the lng option
+        debug: true,
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json'
+        },
+        ns: ['translation'],
+        defaultNS: 'translation'
+    }).then(() => updateContent())
 
-allDatas.forEach(element => {
-    const key = element.getAttribute('data-i18n')
-    element.innerHTML = i18next.t(key)
-})
-   
-
-window.changeLanguage = function(lng){
-    i18next.changeLanguage(lng).then(() => {
-        allDatas.forEach(element => {
-            const key = element.getAttribute('data-i18n')
-            element.innerHTML = i18next.t(key)
-        })        
+    allDatas.forEach(element => {
+        const key = element.getAttribute('data-i18n')
+        element.innerHTML = i18next.t(key)
     })
-    localStorage.setItem('language', lng)
-}
+
+
+    window.changeLanguage = function (lng) {
+        i18next.changeLanguage(lng).then(() => {
+            allDatas.forEach(element => {
+                const key = element.getAttribute('data-i18n')
+                element.innerHTML = i18next.t(key)
+            })
+        })
+        localStorage.setItem('language', lng)
+    }
 }
